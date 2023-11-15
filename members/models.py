a634a8b2ac314ac1c933df8359e58f2a24580b44
Erik Rutledge <erik.rutledge14@gmail.com>
2023-11-15 16:39:57 -0700

    Fixed the database User and Profile models

--- members/models.py
+++ members/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from annoying.fields import AutoOneToOneField
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
  
 class Track(models.Model):
     username = models.ManyToManyField(User)
@@ -15,10 +15,20 @@ class Track(models.Model):
     
 # Extend the User Model by creating a one-to-one link with a Profile
 class Profile(models.Model):
-    user = AutoOneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     friends = models.ManyToManyField(User, related_name='+', blank=True)
     tracks = models.ManyToManyField(Track, blank=True)
 
     def __str__(self):
         return self.user.username
+
+# Construct listeners to create and update a Profile extension on User creation
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
